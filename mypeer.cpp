b4afe2ec41df367496f9b6b70ade6cfe290bfd07
anurag97 <anuragpateriya1997@gmail.com>
2019-12-28 09:39:58 +0530

    complete commit
    Completed project commit

--- mypeer.cpp
+++ mypeer.cpp
@@ -1,86 +1,85 @@
 #include<bits/stdc++.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<stdlib.h>
-#include<myclient.h>
-#include<myserver.h>
-#include<netinet/in.h>
-#include<string.h>
-#include<arpa/inet.h>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+#include <arpa/inet.h>
 using namespace std;
-#define BUFFERSIZE 2048
+
+char traker_Ip[20];
 int tracker_port;
-int server_port;
-char server_IP[20];
-char tracker_IP[20];
-bool mutex_for_server=false;
-char userid[256];
-//map for filename, and filedetails
+
+int PORT;
+char SERVER_IP[20];
+
+bool mutex_server_start=false;
+char * uid = new char [256];
+#define BUFF_SIZE 2048
+
+struct cmp_str
+{
+   bool operator()(char const *a, char const *b) const
+   {
+      return std::strcmp(a, b) < 0;
+   }
+};
 
 
-struct filedetails{
+struct struct_file_details
+{
 	int size;
-	int chunk_count;
-	char *bit_vector;
-}
-struct compare{
-	bool operator()(char const *a, char const *b)const{
-		return strcmp(a,b)<0;
+	int no_of_chunks;
+	char * bit_vector;
+};
+std::map<char * , struct struct_file_details *,cmp_str> map_file_details;
+
+
+
+#include "client.h"
+#include "server.h"
+
+int main(int argc, char **argv)
+{
+
+	if(argc<4)
+	{
+		cout<<"\nDetails missing";
+		return 0;
 	}
-}
-map<char * , struct filedetails*,compare> filedetails_map;
+	strcpy(SERVER_IP,argv[1]);
+	PORT = atoi (argv[2]);
 
+	FILE *fp = fopen ( argv[3]  , "rb+" );	
 
-int main(int argc , char** argv){
-	/* INPUT
-	1. Tracker IP
-	2. Tracker port
-	3. tracker_info.txt
-	*/
+	char buffer[256];
+	memset(buffer,'\0',256);
+	fread( buffer,sizeof(char),256,fp);
 
-	if(argc <4){
-		cout<<"Invalid Input"<<endl;
-		exit(EXIT_SUCCESS);
+	char* token = strtok(buffer, "\n"); 
+	strcpy(traker_Ip,token);
+	token=strtok(NULL,"\n");
+	tracker_port=atoi(token);
+
+	cout<<SERVER_IP<<" "<<PORT<<" "<<traker_Ip<<" "<<tracker_port;
+
+	pthread_t tid_client,tid_server;
+
+	// cout<<"\nEnter tracker IP and PORT";
+	// cin>>traker_Ip>>tracker_port;
+
+	strcpy(uid,"");
+
+	if( pthread_create(&tid_server,NULL,&server,NULL) !=0 ){
+		printf("Failed to create server thread\n");return -1;		
 	}
 
-strcpy(server_IP,argv[1]);
-server_port = atoi(argv[2]);
-FILE *fileptr = fopen(argv[3],"rb+");
+	if( pthread_create(&tid_client,NULL,&client,NULL) !=0 ){
+		printf("Failed to create client thread\n");return -1;
+	}
 
-char buffer[512];
-memset(buffer, '\0',512);
-fread(buffer,sizeof(char),512,fileptr);
+	pthread_join(tid_server, NULL);
 
-
-char * temp  = strtok(buffer,"\n");
-strcpy(tracker_IP,temp);
-temp = strtok(NULL,"\n");
-tracker_port = atoi(temp);
-
-
-
-
-
-//create threads;
-
-pthread_t client_thread_id,server_thread_id;
-
-strcpy(userid ,"");
-
-int server_success = pthread_create(&server_thread_id,NULL, &serverhandler,NULL);
-if(server_success !=0){
-cout<<"Server initialisation error"<<endl;
-return -1;	
-}
-int client_success = pthread_create(&client_thread_id,NULL, &clienthandler,NULL);
-if(client_success !=0){
-cout<<"Client  initialisation error"<<endl;
-return -1;	
-}
-
-
-
-
-pthread_join(server_thread_id,NULL);
-return 0;
+	return 0;				
 }
--- myserver.h
+++ myserver.h
@@ -1,139 +1,175 @@
 #include<bits/stdc++.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<string.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
+#include <arpa/inet.h>
 #define CHUNK_SIZE 512*1024
-
-
-void *requestHandler(void *arg){
-	//new socket fd
-	int sockfd = *((int *)arg);
-
-
-	char filename[256];
-	int interval;
-	int start;
-	bool ackn = true;
-
-	//receive start amd ack
-
-	recv(sockfd, &start,sizeof(start),0);
-	send(sockfd, &ackn, sizeof(ackn),0);
+#define seout cout<<"\nserver: "
 
 
 
-	memset(filename,'\0',sizeof(filename));
+void *serveRequest(void *arg)
+{
 
-	//receive filename and send ackn
+  int sockfd=*((int *)arg);
 
-	recv(sockfd, &filename, sizeof(filename),0)
-    send(sockfd, &ackn, sizeof(ackn),0)
+  char file_name[256];
+  int interval,start_point;
+  bool ack=true;
+
+  
+  recv(sockfd, &start_point, sizeof(start_point),0);
+  send(sockfd,&ack,sizeof(ack),0);
+  seout;cout<<"\nstart_point "<<start_point<<"\n";
+  
+  
+  memset ( file_name , '\0', 256);
+  recv(sockfd, &file_name, sizeof(file_name),0);
+  send(sockfd,&ack,sizeof(ack),0);
+  seout;cout<<"\nRequest for file "<<file_name<<"\n";
 
 
-// recceive interval
+  recv(sockfd, &interval, sizeof(interval),0);
+  send(sockfd,&ack,sizeof(ack),0);
 
-    recv(sockfd, &interval, sizeof(interval),0);
-    send(sockfd,&ackn,sizeof(ackn),0);
+  seout;cout<<"\ninterval "<<interval<<"\n";
+
+  FILE * fp = fopen ( file_name  , "rb" );
+
+  if ( fp == NULL )
+  {
+      printf( "Could not open file test.c" ) ;
+      //return 1;
+  } 
+
+  fseek ( fp , 0 , SEEK_END);
+  int size = ftell ( fp );
+  rewind ( fp );
+
+  auto itr=map_file_details.find(file_name);
+  struct struct_file_details * f = itr->second;
+
+  cout<<"\n"<<f->bit_vector;
+
+  send ( sockfd , &size, sizeof(size), 0);
+  recv(sockfd,&ack,sizeof(ack),0);
+
+  int number_of_chunks=((size-1)/(CHUNK_SIZE))+1;
+
+  send(sockfd,f->bit_vector,strlen(f->bit_vector),0);
+  recv(sockfd,&ack,sizeof(ack),0);
 
 
-    FILE *fileptr = fopen(filename,"rb");
-    if(fileptr ==NULL){
-    	cout<<"Could not open file "<<endl;
+  
+  char bit_vector[number_of_chunks]="";
+  recv(sockfd,&bit_vector,sizeof(bit_vector),0);
+  cout<<"\nno of chunks "<<number_of_chunks;
+  cout<<"\n\tbitvector-";
+
+  for(int i=0;i<number_of_chunks;i++)
+    cout<<bit_vector[i];
+  cout<<"\n";
+
+
+
+  char Buffer[BUFF_SIZE] ; 
+  int n=0;
+  //int i = start_point;
+
+  
+  for(int i=0;i<number_of_chunks;i++)
+  {
+    fseek(fp,i*CHUNK_SIZE,SEEK_SET);
+    char big_Buffer[CHUNK_SIZE];
+
+    if(bit_vector[i]=='1')
+    {
+      seout;cout<<"\n sending i chunk-"<<i;
+      memset(big_Buffer,'\0',CHUNK_SIZE);
+      int no_of_bytes_sended=0;
+      int ack=0;
+      while ( CHUNK_SIZE > no_of_bytes_sended )
+      {
+        //fseek(fp,i*BUFF_SIZE,SEEK_SET);
+
+        if( ( n = fread( Buffer , sizeof(char) , BUFF_SIZE , fp ) ) > 0)
+        {
+          //n = fread( Buffer , sizeof(char) , BUFF_SIZE , fp );
+          seout;cout<<n;
+          send (sockfd , Buffer, n, 0 );
+            seout;cout<<n<<" "<<i<<" "<<ack<<" "<<n<<" "<<no_of_bytes_sended;
+
+            memset ( Buffer , '\0', BUFF_SIZE);
+
+            recv( sockfd , &ack ,sizeof(ack), 0);
+          //size = size - i ;
+            //i+=interval;
+            no_of_bytes_sended+=BUFF_SIZE;
+        }
+        else
+        { seout;cout<<"breaking while loop";
+          break;
+        } //printf("\nSending File  %d \n ",size);
+      }
     }
+  }
+  cout<<"\n Sending file complete";
+  // fclose ( fp ); 
+  
 
-    fseek(fileptr,0,SEEK_END);
-  int size = ftell(fileptr);
-  rewind(fileptr);
-
-  auto ptr = filedetails_map.find(filename)
-  struct filedetails var = ptr->second;
-  cout<<"\n"<<var->bit_vector;
-
-
-  //send size and recv ack
-
-  send(sockfd, &size,sizeof(size),0);
-  recv(sockfd, &ackn,sizeof(ackn),0);
-
-  int chunk_count = ((size-1)/CHUNK_SIZE)-1;
-
-  send(sockfd, &var->bit_vector,sizeof(var->bit_vector),0);
-  recv(sockfd, ackn,sizeof(ackn),0);
-
-  char bit_vector[chunk_count]="";
-  recv(sockfd, &bit_vector,sizeof(bit_vector),0);
-  send(sockfd, &ackn,sizeof(ackn),0);
-
-//print
-char buffer[BUFFERSIZE];
-int n=0;
-
-for(int i =0;i<chunk_count;i++){
-   
-   	fseek(fileptr, i*CHUNK_SIZE,SEEK_SET);
-   	char chunk_buffer[CHUNK_SIZE];
-
-   	if(bit_vector[i]=='1'){
-   		cout<<"sending chunk : "<<i<<endl;
-   		memset(chunk_buffer,'\0',CHUNK_SIZE);
-   		int bytes_sent = 0;
-   		int ackn =0;
-   		while(CHUNK_SIZE >bytes_sent){
-   			if((n=fread(buffer,sizeof(char),BUFFERSIZE,fileptr))>0){
-   				cout<<n;
-   				send(sockfd,buffer,n,0);
-   				memset(buffer,'\0',BUFFERSIZE);
-   				recv(sockfd, &ackn,sizeof(ackn),0);
-   				bytes_sent +=BUFFERSIZE;
-   			}
-   			else{
-   				cout<<"breaking while loop"<<endl;
-					break;
-   			}
-   		}
-   	}
- }
-
-cout<<"Sending file complete!!"<<endl;
-
-close(sockfd);
+  close( sockfd);
 }
 
 
+void * server(void * argv)
+{
+  cout<<"ram";
+
+  //init();
+
+  int server_fd = socket (AF_INET, SOCK_STREAM, 0);
+  
+  pthread_t tid;
+
+  // cout<<"Enter port for process";
+  // cin>>PORT;
+
+  struct sockaddr_in   server_addr,client_addr;
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons( PORT );
+  server_addr.sin_addr.s_addr=inet_addr(SERVER_IP);
 
 
+  cout<<"\nServer port no - "<<ntohs(server_addr.sin_port)<<"\n";
 
-void *serverhandler(void *argv){
-int server_fd = socket(AF_INET,SOCK_STREAM,0);
-pthread_t thread_id;
+  int addrlen = sizeof(sockaddr);
+
+  if(bind (server_fd  , (struct sockaddr *)&server_addr , sizeof ( server_addr ) ) !=0)
+  {
+    seout;cout<<"\nError in Binding Socket\n";
+    exit(1);
+  }
+  mutex_server_start=true;
+  listen (server_fd, 3);
+  cout<<"\nListening";
+  
+  while(1)
+  {
+
+    int sockfd = accept ( server_fd , (struct sockaddr *)&client_addr , (socklen_t*)&addrlen);
+    cout<<"\nport no - "<<ntohs(client_addr.sin_port)<<"\n";
+    cout<<"\nIp add  - "<<inet_ntoa(client_addr.sin_addr);
+    cout<<"\nConnection Established";
+    
+    if( pthread_create(&tid,NULL,&serveRequest,&sockfd) !=0 )
+      printf("Failed to create thread\n");
+
+  }
 
 
-
-struct sockaddr_in server_address,client_address;
-server_address.sin_family = AF_INET;
-server_address.sin_port = htons(server_port);
-server_address.sin_addr.s_addr = inet_addr(server_IP);
-
-int addrlen = sizeof(sockaddr);
-cout<<"Server port : "<<ntohs(server_address.sin_port)<<endl;
-
-if(bind(server_fd,(struct sockaddr *)&server_address,sizeof(server_address))!=0){
-     cout<<"\nError in binding socket\n";
-     exit(1);
-}
-mutex_for_server = true;
-listen(server_fd,3);
-cout<<"\nServer is Listening....."<<endl;
-
-
-while(true){
-	int sockfd = accept(server_fd, (struct sockaddr*)&client_address,(socklen_t *)&addrlen);
-    cout<<"\nclient Port no : "<<ntohs(client_address.sin_port)<<endl;
-    cout<<"\nclient Ip addre : "<<inet_ntoa(client_address.sin_addr)<<endl;
-     cout<<"Connection established"<<endl;  
-}
-
-close(server_fd);
+  close( server_fd);  
 }
